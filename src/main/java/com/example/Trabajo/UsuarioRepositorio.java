cac3d7d13460ab1541ec115114013eaedd5d7bfb
AlvaroSalcedoRamirez <aesalcedo@unicesar.edu.co>
2021-12-07 18:19:46 -0500

    Parcial Finalizado 3.0

--- src/main/java/com/example/Trabajo/UsuarioRepositorio.java
+++ src/main/java/com/example/Trabajo/UsuarioRepositorio.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.Trabajo;
 
 import java.util.List;
 import org.springframework.data.repository.Repository;
 
-/**
- *
- * @author KGC
- */
 public interface UsuarioRepositorio extends Repository <Usuario, Integer> {
     List<Usuario>findAll();
     Usuario findById(int id);
--- src/main/java/com/example/Trabajo/UsuarioServiceIMP.java
+++ src/main/java/com/example/Trabajo/UsuarioServiceIMP.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.Trabajo;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- *
- * @author KGC
- */
+
 @Service
 public class UsuarioServiceIMP implements UsuarioService {
 
