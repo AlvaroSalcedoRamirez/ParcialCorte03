8547b82884dcd533367ae30b9eb82d47b4f7297a
AlvaroSalcedoRamirez <aesalcedo@unicesar.edu.co>
2021-12-07 18:17:21 -0500

    Parcial Finalizado 2.0

--- src/main/java/com/example/Trabajo/Controlador.java
+++ src/main/java/com/example/Trabajo/Controlador.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
 @RequestMapping({"/usuarios"})
 public class Controlador {
