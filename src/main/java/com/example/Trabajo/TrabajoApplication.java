d80c41e7d8fe2230ec48f70df47ef1144a7a3a1f
AlvaroSalcedoRamirez <aesalcedo@unicesar.edu.co>
2021-12-07 18:06:03 -0500

    Trabajo finalizado

--- src/main/java/com/example/Trabajo/TrabajoApplication.java
+++ src/main/java/com/example/Trabajo/TrabajoApplication.java
@@ -23,7 +23,7 @@ public class TrabajoApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
